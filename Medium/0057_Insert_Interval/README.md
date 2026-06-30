a1fda674b79a1e9720ef9a2e5b1c7b20698c515a
Suraj Singh <suraj14mk@gmail.com>
2026-06-30 21:36:19 +0530

    Solved 57. Insert Interval

--- Medium/0057_Insert_Interval/README.md
+++ Medium/0057_Insert_Interval/README.md
@@ -4,9 +4,9 @@
 - **Difficulty:** Medium
 - **Tags:** Array
 - **Language:** C++
-- **Date Solved:** 2026-06-30 21:08:33
-- **Runtime:** 8 ms (Beats 7.19%)
-- **Memory:** 21.6 MB (Beats 94.85%)
+- **Date Solved:** 2026-06-30 21:33:49
+- **Runtime:** 0 ms (Beats 100.00%)
+- **Memory:** 21.7 MB (Beats 47.60%)
 
 ## Problem Link
 [Official LeetCode URL](https://leetcode.com/problems/insert-interval/)
@@ -20,3 +20,4 @@ Submission generated automatically by LeetSync.
 ## Submission History
 - **v1**: C++ on 2026-06-30 21:08:18 (Runtime: 7 ms, Memory: 21.9 MB)
 - **v2**: C++ on 2026-06-30 21:08:33 (Runtime: 8 ms, Memory: 21.6 MB)
+- **v3**: C++ on 2026-06-30 21:33:49 (Runtime: 0 ms, Memory: 21.7 MB)
---
+++ Medium/0057_Insert_Interval/v3_solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>>merge;
+         int n=intervals.size();
+         int i=0;
+         for(;i<n;i++){
+            if(newInterval[0]>intervals[i][1]){
+                merge.push_back(intervals[i]);
+            }else break;
+         }
+         while(i<n&&intervals[i][0]<=newInterval[1]){
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1],intervals[i][1]);
+            i++;
+         }
+         merge.push_back(newInterval);
+         while(i<n){
+            merge.push_back(intervals[i]);
+            i++;
+         }
+
+
+         return merge;
+
+    }
+};
