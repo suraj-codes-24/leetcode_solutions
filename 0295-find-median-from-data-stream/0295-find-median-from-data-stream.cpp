189f01163c396777b6f7bd6b9ac743d167284ca7
Suraj Singh <suraj14mk@gmail.com>
2026-07-06 22:50:25 +0530

    Time: 76 ms (55.48%), Space: 148.6 MB (29.63%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -0,0 +1,45 @@
+class MedianFinder {
+public:
+    priority_queue<int> left_max_heap;
+    priority_queue<int, vector<int>, greater<int>>right_min_heap;
+    MedianFinder() {
+        while(! left_max_heap.empty()) left_max_heap.pop();
+        while(! right_min_heap.empty()) right_min_heap.pop();
+    }
+    
+    void addNum(int num) {
+        if(left_max_heap.empty()){
+            left_max_heap.push(num);
+        }
+        else if(left_max_heap.top()>num){
+            left_max_heap.push(num);
+        }
+        else{
+            right_min_heap.push(num);
+        }
+       if(abs((int)(left_max_heap.size()-right_min_heap.size()))>1){
+        right_min_heap.push(left_max_heap.top());
+        left_max_heap.pop();
+       }
+       else if(left_max_heap.size()<right_min_heap.size()){
+        left_max_heap.push(right_min_heap.top());
+        right_min_heap.pop();
+       }
+    }
+    
+    double findMedian() {
+        if((left_max_heap.size()+right_min_heap.size())%2!=0){
+            return (double)left_max_heap.top();
+        }
+        else{
+            return (left_max_heap.top()+right_min_heap.top())/2.0;
+        }
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
