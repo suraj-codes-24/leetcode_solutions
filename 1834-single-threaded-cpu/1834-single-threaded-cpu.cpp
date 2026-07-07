1db7ef2b3a0a7a4f8708101c9127105cdf008ee1
Suraj Singh <suraj14mk@gmail.com>
2026-07-07 23:58:51 +0530

    Time: 137 ms (58.62%), Space: 133.8 MB (69.19%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
     vector<int> getOrder(vector<vector<int>>& tasks) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
-        long long time=0;
-        vector<int>ans;
-        int t=0;
-         for(auto &x:tasks){
-            x.push_back(t);
-            t++;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+
+        int n = tasks.size();
+        vector<int> ans;
+
+        // store original index
+        for(int i = 0; i < n; i++) {
+            tasks[i].push_back(i);
         }
-        sort(tasks.begin(),tasks.end());
-        for(auto x:tasks){
-            cout<<x[0]<<" "<<x[1]<<" "<<x[2]<<"\n";
-        }
-       for(int i=0;i<tasks.size();){
-            if(time<tasks[i][0]){
-                time=tasks[i][0];
+
+        // sort by enqueue time
+        sort(tasks.begin(), tasks.end());
+
+        long long time = 0;
+        int i = 0;
+
+        while(i < n || !pq.empty()) {
+
+            // if no task is available, jump time to next task's enqueue time
+            if(pq.empty() && time < tasks[i][0]) {
+                time = tasks[i][0];
             }
-            while(i < tasks.size() && time >= tasks[i][0]){
-                pq.push({tasks[i][1],tasks[i][2]});
+
+            // add all available tasks
+            while(i < n && time >= tasks[i][0]) {
+                pq.push({tasks[i][1], tasks[i][2]});
                 i++;
             }
-            while(!pq.empty()){
-                auto [b,idx]=pq.top();
-                pq.pop();
-                time+=b;
-                ans.push_back(idx);
-                while(i < tasks.size() && time >= tasks[i][0]){
-                pq.push({tasks[i][1],tasks[i][2]});
-                i++;
-                }
-                
-            }
-       }
-       return ans;
-        
+
+            // execute task with smallest processing time
+            auto [processTime, index] = pq.top();
+            pq.pop();
+
+            time += processTime;
+            ans.push_back(index);
+        }
+
+        return ans;
     }
 };
