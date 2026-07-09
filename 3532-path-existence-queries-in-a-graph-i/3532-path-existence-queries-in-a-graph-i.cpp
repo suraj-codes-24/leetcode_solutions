62dfbcb5e142669a5163fe9e46143dd2e7c4fca4
Suraj Singh <suraj14mk@gmail.com>
2026-07-09 21:19:25 +0530

    Time: 57 ms (27.94%), Space: 235.8 MB (29.04%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
-    void dfs(int curr,vector<int>&component,int com,vector<vector<int>>&adj){
-        component[curr]=com;
-        for(auto neb:adj[curr]){
-            if(component[neb]==-1){
-                dfs(neb,component,com,adj);
-            }
-        }
-    }
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
         
-        vector<vector<int>>adj(n);
-
+        
+        vector<int>component(n,-1);
+        component[0]=1;
         for(int i=1;i<n;i++){
             if(nums[i]-nums[i-1]<=maxDiff){
-                adj[i-1].push_back(i);
-                adj[i].push_back(i-1);
+                component[i]=component[i-1];
             }
-        }
-        vector<int>component(n,-1);
-        int com=1;
-        for(int i=0;i<n;i++){
-            if(component[i]==-1){
-                dfs(i,component,com,adj);
-                com++;
-            }
+            else
+            component[i]=component[i-1]+1;
         }
         vector<bool>ans;
         for(auto q:queries){
