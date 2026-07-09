71469150fdad4e0bdd135779692bcf19743d5382
Suraj Singh <suraj14mk@gmail.com>
2026-07-09 22:22:07 +0530

    Time: 29 ms (9.49%), Space: 24.3 MB (8.78%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -3,22 +3,22 @@ public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n=matrix.size();
        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<>>pq;
-       vector<vector<int>>visited(n,vector<int>(n,false));
        pq.push({matrix[0][0],0,0});
-       visited[0][0] = true;
+       unordered_set<int>set;
+       set.insert(0);
 
        while(!pq.empty()){
             auto [val,r,c]=pq.top();
             pq.pop();
             k--;
             if(k==0)return val;
-            if(r+1<n&&!visited[r+1][c]){
-            pq.push({matrix[r+1][c],r+1,c});
-            visited[r+1][c] = true;
+            if(r+1<n&&!set.count((r+1)*n+c)){
+                pq.push({matrix[r+1][c],r+1,c});
+                set.insert((r+1)*n+c);
             }
-            if(c+1<n&&!visited[r][c+1]){
+            if(c+1<n&&!set.count((r*n)+c+1)){
                 pq.push({matrix[r][c+1],r,c+1});
-                visited[r][c+1] = true;
+                set.insert((r*n)+c+1);
             }
         }
         return 0;
