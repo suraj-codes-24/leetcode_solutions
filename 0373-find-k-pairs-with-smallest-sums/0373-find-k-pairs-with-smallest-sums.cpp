cabbe8767cabf7137287ba738d0de7a30bb8e76c
Suraj Singh <suraj14mk@gmail.com>
2026-07-11 18:22:18 +0530

    Time: 85 ms (53.15%), Space: 165.3 MB (38.61%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
+    // Custom hash for pair<int, int>
+    struct PairHash {
+        size_t operator()(const pair<int, int>& p) const {
+            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+        }
+    };
+
     vector<vector<int>> kSmallestPairs(vector<int>& nums1,
                                        vector<int>& nums2,
                                        int k) {
 
-        // Min Heap
-        // {sum, {index in nums1, index in nums2}}
         priority_queue<
             pair<int, pair<int, int>>,
             vector<pair<int, pair<int, int>>>,
@@ -15,48 +20,40 @@ public:
         int n = nums1.size();
         int m = nums2.size();
 
-        vector<vector<int>> answer;
+        vector<vector<int>> ans;
 
-        // Keeps track of index pairs already pushed into heap
-        set<pair<int, int>> visited;
+        // unordered_set with custom hash
+        unordered_set<pair<int, int>, PairHash> visited;
 
-        // Start with the smallest possible pair
+        // Start from (0,0)
         minHeap.push({nums1[0] + nums2[0], {0, 0}});
         visited.insert({0, 0});
 
         while (k > 0 && !minHeap.empty()) {
 
-            // Get the smallest sum pair
             auto current = minHeap.top();
             minHeap.pop();
 
             int i = current.second.first;
             int j = current.second.second;
 
-            // Store the actual values
-            answer.push_back({nums1[i], nums2[j]});
+            ans.push_back({nums1[i], nums2[j]});
 
-            // Move down (next element in nums1)
+            // Move down
             if (i + 1 < n && !visited.count({i + 1, j})) {
-                minHeap.push({
-                    nums1[i + 1] + nums2[j],
-                    {i + 1, j}
-                });
+                minHeap.push({nums1[i + 1] + nums2[j], {i + 1, j}});
                 visited.insert({i + 1, j});
             }
 
-            // Move right (next element in nums2)
+            // Move right
             if (j + 1 < m && !visited.count({i, j + 1})) {
-                minHeap.push({
-                    nums1[i] + nums2[j + 1],
-                    {i, j + 1}
-                });
+                minHeap.push({nums1[i] + nums2[j + 1], {i, j + 1}});
                 visited.insert({i, j + 1});
             }
 
             k--;
         }
 
-        return answer;
+        return ans;
     }
 };
