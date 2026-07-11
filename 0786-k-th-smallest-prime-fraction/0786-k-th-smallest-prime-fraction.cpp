f30b081d31f6cd8f1a606936b8d399da7ca39526
Suraj Singh <suraj14mk@gmail.com>
2026-07-11 22:39:32 +0530

    Time: 303 ms (41.82%), Space: 65.8 MB (44.34%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        vector<tuple<double,int,int>> frac;
+        priority_queue<tuple<double,int,int>> pq;
         int n = arr.size();
 
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
-                frac.push_back({1.0 * arr[i] / arr[j], i, j});
+                pq.push({1.0 * arr[i] / arr[j], i, j});
+                if(pq.size()>k)pq.pop();
             }
         }
 
-        sort(frac.begin(), frac.end());
-
-        auto [value, i, j] = frac[k - 1];
+        auto [value, i, j] = pq.top();
         return {arr[i], arr[j]};
     }
 };
