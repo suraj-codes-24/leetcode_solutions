b970286151ba9000cbd8f69a89273be74c927c8b
Suraj Singh <suraj14mk@gmail.com>
2026-07-12 21:54:17 +0530

    Time: 100 ms (15.87%), Space: 42.6 MB (13.37%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -3,19 +3,19 @@ public:
 const int MOD=1e9+7;
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         priority_queue<long long>pq;
+        vector<long long>vec;
         for(int i=0;i<n;i++){
             long long sum=0;
             for(int j=i;j<n;j++){
                 sum=(sum+nums[j])%MOD;
-                pq.push(sum);
-                if(pq.size()>right)pq.pop();
+               vec.push_back(sum);
             }
         }
+        sort(vec.begin(),vec.end());
         long long ans=0;
-        while(pq.size()>=left){
-            ans=(ans+pq.top())%MOD;
-            pq.pop();
-        }
+       for(int i=left-1;i<right;i++){
+        ans=(ans+vec[i])%MOD;
+       }
         return  ans;
         
     }
