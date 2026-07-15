caea2359e9fdcfa7b3d8914d821914323bb37295
Suraj Singh <suraj14mk@gmail.com>
2026-07-15 16:26:11 +0530

    Time: 1281 ms (20.87%), Space: 296.2 MB (27.83%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,31 +1,64 @@
 class Solution {
 public:
-int dp[201][201][201];
+// int dp[201][201][201];
 static const int MOD=1e9+7;
     int gcd(int a, int b) {
         if (b == 0) return a;
         return gcd(b, a % b);
 }   
-    int solve(vector<int>&nums,int i,int first,int second){
-        if(i==nums.size()){
-           bool both_nonempty=first!=0&&second!=0;
-           bool equal=first==second;
-           if(both_nonempty&&equal)return 1;
-           return 0;
+    // int solve(vector<int>&nums,int i,int first,int second){
+    //     if(i==nums.size()){
+    //        bool both_nonempty=first!=0&&second!=0;
+    //        bool equal=first==second;
+    //        if(both_nonempty&&equal)return 1;
+    //        return 0;
 
-        }
-        if(dp[i][first][second]!=-1)return dp[i][first][second];
-        int skip=(solve(nums,i+1,first,second))%MOD;
-        int seq1=(solve(nums,i+1,gcd(first,nums[i]),second))%MOD;
-        int seq2=(solve(nums,i+1,first,gcd(second,nums[i])))%MOD;
-        long long ans=skip;
-        ans=(ans+seq1)%MOD;
-        ans=(ans+seq2)%MOD;
-        return dp[i][first][second]=ans;
-    }
+    //     }
+    //     if(dp[i][first][second]!=-1)return dp[i][first][second];
+    //     int skip=(solve(nums,i+1,first,second))%MOD;
+    //     int seq1=(solve(nums,i+1,gcd(first,nums[i]),second))%MOD;
+    //     int seq2=(solve(nums,i+1,first,gcd(second,nums[i])))%MOD;
+
+    //     return dp[i][first][second]=(skip%MOD+seq1%MOD+seq2%MOD)%MOD;
+    // }
     int subsequencePairCount(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,0,0,0);
+        // memset(dp,-1,sizeof(dp));
+        // return solve(nums,0,0,0);
+        int size=nums.size();
+        int maxgcd=nums[0];
+
+        for(int i=1;i<size;i++){
+            maxgcd=max(maxgcd,nums[i]);
+        }
+
+        vector<vector<vector<int>>>dp(size+1,vector<vector<int>>(maxgcd+1,vector<int>(maxgcd+1,0)));
+
+        for(int i=0;i<maxgcd+1;i++){
+            for(int j=0;j<maxgcd+1;j++){
+                bool notempty=i!=0&&j!=0;
+                bool equal=i==j;
+                if(notempty&&equal){
+                    dp[size][i][j]=1;
+                }
+            }
+        }
+        for(int i=size-1;i>=0;i--){
+            for(int j=0;j<maxgcd+1;j++){
+                for(int k=0;k<maxgcd+1;k++){
+                    int skip=dp[i+1][j][k];
+                    int take1=dp[i+1][gcd(j,nums[i])][k];
+                    int take2=dp[i+1][j][gcd(k,nums[i])];
+
+                    long long ans=skip;
+                    ans=(ans+take1)%MOD;
+                    ans=(ans+take2)%MOD;
+                    dp[i][j][k]=ans;
+
+                }
+            }
+        }
+        return dp[0][0][0];
+
         
     }
 };
