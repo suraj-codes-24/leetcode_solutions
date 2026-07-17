603fc2f19aa963ae4399bce0faf67edbbfe80654
Suraj Singh <suraj14mk@gmail.com>
2026-07-17 12:52:17 +0530

    Time: 12 ms (55.93%), Space: 58.1 MB (75.79%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int n=heights.size();
-        priority_queue<int,vector<int>,greater<>>pq;
-        for(int i=1;i<n;i++){
-           int currdiff=heights[i]-heights[i-1];
-           if(currdiff>0){
+        int n = heights.size();
+        
+        // Min-heap to keep track of all the jumps where we've used a ladder.
+        // It will keep the smallest jumps at the top.
+        priority_queue<int, vector<int>, greater<>> pq;
+        
+        for(int i = 1; i < n; i++) {
+           int currdiff = heights[i] - heights[i-1];
+           
+           // Step 1: The Optimistic Move
+           // If we need to jump up, automatically assume we use a ladder.
+           if(currdiff > 0) {
             pq.push(currdiff);
            }
-           if(pq.size()>ladders){
-            int diff=pq.top();
+           
+           // Step 2: The Reality Check
+           // If the number of jumps in our heap exceeds our actual ladder count,
+           // we need to retroactively pay for the smallest jump using bricks.
+           if(pq.size() > ladders) {
+            
+            // Extract the smallest jump we've made so far
+            int diff = pq.top();
             pq.pop();
-            if(diff<=bricks){
-                bricks-=diff;
+            
+            // Step 3: The Fix
+            // Do we have enough bricks to cover this smallest jump?
+            if(diff <= bricks) {
+                bricks -= diff;
+            } 
+            // If we don't even have enough bricks for the smallest jump, 
+            // we are completely out of resources. Game over.
+            else {
+                return i - 1;
             }
-            else
-            return i-1;
            }
         }
-        return n-1;
+        
+        // If the loop finishes, we successfully reached the final building!
+        return n - 1;
     }
 };
