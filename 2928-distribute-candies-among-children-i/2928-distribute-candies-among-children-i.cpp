427daad3db409f977a03b3194b6f794e628588ad
Suraj Singh <suraj14mk@gmail.com>
2026-07-27 21:54:06 +0530

    Time: 16 ms (7.88%), Space: 8.3 MB (91.44%) - LeetHub

---
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int distributeCandies(int n, int l) {
+        int c=0;
+        for(int i=0;i<=l;i++){
+            for(int j=0;j<=l;j++){
+                for(int k=0;k<=l;k++){
+                    if(i+k+j==n)c++;
+                }
+            }
+        }
+        return c;
+    }
+};
