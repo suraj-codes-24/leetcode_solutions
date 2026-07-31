0ca037dacb540c4df5b9940f26f8fbdb8e5f9607
Suraj Singh <suraj14mk@gmail.com>
2026-07-31 19:19:00 +0530

    Time: 679 ms (17.95%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -7,14 +7,9 @@ where primary_flag = "Y"
 
 union
 
-select
-e.employee_id,
-e.department_id
-from
-(select
-employee_id,
-count(employee_id) as ct
-from employee
-group by employee_id) as t
-join employee as e
-on t.ct=1 and e.employee_id=t.employee_id
+SELECT
+    employee_id,
+    MIN(department_id) AS department_id
+FROM employee
+GROUP BY employee_id
+HAVING COUNT(*) = 1
