d0b0282492ff6008a150eac1f57c986a3fa2b18a
Suraj Singh <suraj14mk@gmail.com>
2026-08-03 15:28:22 +0530

    Time: 8 ms (32.71%), Space: 11.5 MB (38.63%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
+    int dp[501][501];
+    int solve(vector<int>& piles ,int i,int j){
+        if(i>=j){
+            return 0;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        int x=piles[i]-solve(piles,i+1,j);
+        int y=piles[j]-solve(piles,i,j-1);
+        return dp[i][j]=max(x,y);
+
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        memset(dp,-1,sizeof(dp));
+        int n=piles.size();
+        int x=solve(piles,0,n-1);
+        if(x) return true;
+        else
+        return false;
     }
 };
