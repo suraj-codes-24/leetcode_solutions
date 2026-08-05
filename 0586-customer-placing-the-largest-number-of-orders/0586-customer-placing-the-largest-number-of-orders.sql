01363c50233c63173834fcbed1d169c8cd0d1184
Suraj Singh <suraj14mk@gmail.com>
2026-08-05 16:13:12 +0530

    Time: 462 ms (77.99%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,11 +1,6 @@
-# Write your MySQL query statement below
-select
-t.customer_number
-from
-(select
-customer_number,
-count(customer_number) as freq
-from orders
-group by customer_number
-order by freq desc) as t
-limit 1;
+SELECT
+    customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1;
