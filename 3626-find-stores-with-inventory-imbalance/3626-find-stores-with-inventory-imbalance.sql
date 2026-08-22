7534d3e1d5f81a5fed4dc5adb110e8d7edf0fed9
Suraj Singh <suraj14mk@gmail.com>
2026-08-23 01:53:36 +0530

    Time: 555 ms (75.16%), Space: 0B (100%) - LeetHub

--- 3626-find-stores-with-inventory-imbalance/3626-find-stores-with-inventory-imbalance.sql
+++ 3626-find-stores-with-inventory-imbalance/3626-find-stores-with-inventory-imbalance.sql
@@ -1,49 +1,63 @@
-select
-s.store_id,
-s.store_name,
-s.location,
-c.most_exp_product ,
-c.cheapest_product ,
-round(c.cheapest_product_quantity/c.most_exp_product_quantity ,2)as imbalance_ratio  
-from stores as s
-natural join
-(select
-*
-from
-(select
-x.store_id,
-x.product_name  as most_exp_product ,
-y.most_exp_product_price,
-x.quantity as most_exp_product_quantity
-from inventory x
-natural join
-(select
-store_id,
-max(price) as most_exp_product_price   
-from inventory
-group by store_id
-having count(store_id)>=3)y
-where x.price=y.most_exp_product_price) as a
-
-natural join
-
-
-(select
-x.store_id,
-x.product_name  as cheapest_product,
-y.cheapest_price,
-x.quantity as cheapest_product_quantity
-from inventory x
-natural join
-(select
-store_id,
-min(price) as cheapest_price   
-from inventory
-group by store_id
-having count(store_id)>=3)y
-where x.price=y.cheapest_price) as b)c
-where c.most_exp_product_quantity<c.cheapest_product_quantity
-order by imbalance_ratio desc, s.store_name
-
-
-
+SELECT
+    s.store_id,
+    s.store_name,
+    s.location,
+    c.most_exp_product,
+    c.cheapest_product,
+    ROUND(
+        c.cheapest_product_quantity /
+        NULLIF(c.most_exp_product_quantity,0),
+        2
+    ) AS imbalance_ratio
+FROM stores s
+JOIN
+(
+    SELECT
+        a.store_id,
+        a.most_exp_product,
+        a.most_exp_product_quantity,
+        b.cheapest_product,
+        b.cheapest_product_quantity
+    FROM
+    (
+        SELECT
+            x.store_id,
+            x.product_name AS most_exp_product,
+            x.quantity AS most_exp_product_quantity
+        FROM inventory x
+        JOIN
+        (
+            SELECT
+                store_id,
+                MAX(price) AS most_exp_product_price
+            FROM inventory
+            GROUP BY store_id
+            HAVING COUNT(*) >= 3
+        ) y
+        ON x.store_id = y.store_id
+        AND x.price = y.most_exp_product_price
+    ) a
+    JOIN
+    (
+        SELECT
+            x.store_id,
+            x.product_name AS cheapest_product,
+            x.quantity AS cheapest_product_quantity
+        FROM inventory x
+        JOIN
+        (
+            SELECT
+                store_id,
+                MIN(price) AS cheapest_price
+            FROM inventory
+            GROUP BY store_id
+            HAVING COUNT(*) >= 3
+        ) y
+        ON x.store_id = y.store_id
+        AND x.price = y.cheapest_price
+    ) b
+    ON a.store_id = b.store_id
+) c
+ON s.store_id = c.store_id
+WHERE c.most_exp_product_quantity < c.cheapest_product_quantity
+ORDER BY imbalance_ratio DESC, s.store_name;
