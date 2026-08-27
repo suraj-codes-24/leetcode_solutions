add7409455ae68c8d7877ac16c2b1a568a2eac91
Suraj Singh <suraj14mk@gmail.com>
2026-08-27 15:14:21 +0530

    Time: 455 ms (91.86%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,18 +1,20 @@
-# Write your MySQL query statement below
-select
-p.product_id,
-ifnull(b.price,10) as price
-from
-(select distinct
-a.product_id ,
-first_value(a.new_price) over(partition by a.product_id order by a.change_date desc)as price
-from
-(select
-product_id ,
-new_price ,
-change_date
-from products 
-where change_date <='2019-08-16'
-)a)b
-right join (select distinct product_id from products)p
-on p.product_id=b.product_id
+SELECT
+    p.product_id,
+    IFNULL(b.price, 10) AS price
+FROM
+(
+    SELECT DISTINCT
+        product_id,
+        FIRST_VALUE(new_price) OVER (
+            PARTITION BY product_id
+            ORDER BY change_date DESC
+        ) AS price
+    FROM products
+    WHERE change_date <= '2019-08-16'
+) b
+RIGHT JOIN
+(
+    SELECT DISTINCT product_id
+    FROM products
+) p
+ON p.product_id = b.product_id;
