1bb18b7d831644ea98759c6b06f2dd0785c9ece7
Suraj Singh <suraj14mk@gmail.com>
2026-08-31 18:51:58 +0530

    Time: 80 ms (13.55%), Space: 8.1 MB (46.36%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
-    void dfs(vector<int>&nums,vector<bool>&avail,vector<int>&perm,int & count){
-        if(perm.size()==nums.size()){
+    void dfs(vector<int>& nums, vector<bool>& available,
+             vector<int>& permutation, int& count) {
+
+        if (permutation.size() == nums.size()) {
             count++;
+            return;
         }
-        else{
-            for(int i=0;i<nums.size();i++){
-                if(avail[i]){
-                    if((nums[i]%(perm.size()+1)==0||(perm.size()+1)%nums[i]==0)){
-                    perm.push_back(nums[i]);
-                    avail[i]=false;
-                    dfs(nums,avail,perm,count);
-                    avail[i]=true;
-                    perm.pop_back();
-                    }
-                    
+
+        int position = permutation.size() + 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (available[i]) {
+
+                // Beautiful arrangement condition
+                if (nums[i] % position == 0 || position % nums[i] == 0) {
+
+                    permutation.push_back(nums[i]);
+                    available[i] = false;
+
+                    dfs(nums, available, permutation, count);
+
+                    available[i] = true;
+                    permutation.pop_back();
                 }
             }
         }
     }
-   int countArrangement(int n) {
-        vector<int>nums(n);
-		for(int i=0;i<n;i++)nums[i]=i+1;
-        vector<int>perm;
-        vector<bool>avail(n,true);
-        int count=0;
-        dfs(nums,avail,perm,count);
+
+    int countArrangement(int n) {
+        vector<int> nums(n);
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = i + 1;
+        }
+
+        vector<int> permutation;
+        vector<bool> available(n, true);
+
+        int count = 0;
+
+        dfs(nums, available, permutation, count);
+
         return count;
-        
     }
 };
