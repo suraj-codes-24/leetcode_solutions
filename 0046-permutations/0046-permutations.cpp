2781de07adece1545789a54ef53ff1ce221c2ba1
Suraj Singh <suraj14mk@gmail.com>
2026-08-31 18:22:04 +0530

    Time: 3 ms (25.82%), Space: 10.6 MB (51.27%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void dfs(vector<int>&nums,vector<bool>&avail,vector<int>&perm,vector<vector<int>>&all){
+        if(perm.size()==nums.size()){
+            all.push_back(perm);
+        }
+        else{
+            for(int i=0;i<nums.size();i++){
+                if(avail[i]){
+                    perm.push_back(nums[i]);
+                    avail[i]=false;
+                    dfs(nums,avail,perm,all);
+                    avail[i]=true;
+                    perm.pop_back();
+                }
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>all;
+        vector<int>perm;
+        vector<bool>avail(n,true);
+        dfs(nums,avail,perm,all);
+        return all;
+        
+    }
+};
