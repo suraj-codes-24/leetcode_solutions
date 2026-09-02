b83000c0db34a04364a5a815bc5de62f7f4e69f2
Suraj Singh <suraj14mk@gmail.com>
2026-09-02 19:04:32 +0530

    Time: 37 ms (73.57%), Space: 62.5 MB (85.62%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int N,K;
-    void dfs(int val,vector<int>&temp,vector<vector<int>>&ans){
-        if(temp.size()==K)
-        ans.push_back(temp);
+    void dfs(int start, int n, int k,
+             vector<int>& temp,
+             vector<vector<int>>& ans) {
 
-        for(int j=val;j<=N;j++){
+        if(temp.size() == k) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for(int j = start; j <= n; j++) {
             temp.push_back(j);
-            dfs(j+1,temp,ans);
+
+            dfs(j + 1, n, k, temp, ans);
+
             temp.pop_back();
         }
     }
-    vector<vector<int>> subsets() {
-        vector<vector<int>> ans;
-        vector<int>temp;
-        dfs(1,temp,ans);
-        return ans;
-    }    
 
     vector<vector<int>> combine(int n, int k) {
-        N=n;
-        K=k;
-        return subsets();
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        dfs(1, n, k, temp, ans);
+
+        return ans;
     }
 };
