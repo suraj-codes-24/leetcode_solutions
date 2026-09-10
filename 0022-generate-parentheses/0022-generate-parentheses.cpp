eb3fcc968a0b76448e15d481783524d4829d8afe
Suraj Singh <suraj14mk@gmail.com>
2026-09-10 10:05:23 +0530

    Time: 2 ms (79.8%), Space: 13 MB (89.06%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void dfs(int n,string &par,vector<string> &ans,int left_par_count,int right_par_count){
+        if(par.size()==2*n&&left_par_count==right_par_count) {
+            ans.push_back(par); 
+            return ;
+        }
+        else if(par.size()==2*n) return;
+        if(left_par_count>=right_par_count){
+            par.push_back('(');
+            dfs(n,par,ans,left_par_count+1,right_par_count);
+            par.pop_back();
+            par.push_back(')');
+            dfs(n,par,ans,left_par_count,right_par_count+1);
+            par.pop_back();
+        }
+        
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        string par="";
+        dfs(n,par,ans,0,0);
+        return ans;
+    }
+};
