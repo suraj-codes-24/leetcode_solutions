654bfd60d39a84bf23b20e6a7a92f8d224fab373
Suraj Singh <suraj14mk@gmail.com>
2026-09-21 18:50:10 +0530

    Time: 30 ms (41.93%), Space: 27.5 MB (57.52%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -27,7 +27,7 @@ public:
         int n=text1.size();
         int m=text2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-
+        int ans=0;
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
                 if(text1[i]==text2[j]){
@@ -36,9 +36,10 @@ public:
                 else{
                     dp[i][j]=max(dp[i+1][j],dp[i][j+1]);
                 }
+                ans=max(dp[i][j],ans);
             }
         }
-        return dp[0][0];
+        return ans;
         
     }
 };
