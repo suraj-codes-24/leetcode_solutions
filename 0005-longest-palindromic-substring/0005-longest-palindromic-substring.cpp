b52769002cd61edf7d92783517a87c2f3254d228
Suraj Singh <suraj14mk@gmail.com>
2026-09-21 18:13:42 +0530

    Time: 1010 ms (5.29%), Space: 291.9 MB (6.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    bool ispalindrome(string &s ,int i, int j){
-        while(i<j){
-            if(s[i]!=s[j]) return false;
+    bool isPalindrome(string &s, int i, int j) {
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
+
             i++;
             j--;
         }
+
         return true;
     }
-    int dp[1001][1001];
+
     string longestPalindrome(string s) {
-        
-        int length=-1;
-        int st=-1;
-        memset(dp,-1,sizeof(dp));
-        for(int i=0;i<s.size();i++){
-            for(int j=i;j<s.size();j++){
-                bool palindrome=false;
-                if(dp[i][j]!=-1){
-                    palindrome=dp[i][j];
-                }
-                else{
-                    palindrome=ispalindrome(s,i,j);
-                }
-                if(palindrome){
-                    dp[i][j]=true;
-                    if(j-i+1>length){
-                        length=j-i+1;
-                        st=i;
-                    }
+        int n = s.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        int start = 0;
+        int length = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                if (dp[i][j] == -1)
+                    dp[i][j] = isPalindrome(s, i, j);
+
+                if (dp[i][j] && j - i + 1 > length) {
+                    start = i;
+                    length = j - i + 1;
                 }
             }
         }
-        return s.substr(st,length);
+
+        return s.substr(start, length);
     }
 };
