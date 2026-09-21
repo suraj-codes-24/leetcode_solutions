8a36b77557c82b689820ebd13ff2a4d19658569e
Suraj Singh <suraj14mk@gmail.com>
2026-09-21 10:23:55 +0530

    Time: 131 ms (2.42%), Space: 287.5 MB (5.02%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int dp[101][10001];
-    bool solve(string s, string t,int i, int j){
-        if(i>=s.size()) return true;
-        if(j>=t.size()) return false;
 
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]){
-           return dp[i][j]=solve(s,t,i+1,j+1);
-        }
-        else{
-            return dp[i][j]=solve(s,t,i,j+1);
-        }
+    bool solve(string s, string t, int i, int j) {
+        if (i >= s.size()) return true;
+        if (j >= t.size()) return false;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s[i] == t[j])
+            return dp[i][j] = solve(s, t, i + 1, j + 1);
+
+        return dp[i][j] = solve(s, t, i, j + 1);
     }
+
     bool isSubsequence(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return solve(s,t,0,0);
+        memset(dp, -1, sizeof(dp));
+        return solve(s, t, 0, 0);
     }
 };
