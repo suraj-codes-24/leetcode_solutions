85aa31925b6aa290ca884a4e1039153e5c438c92
Suraj Singh <suraj14mk@gmail.com>
2026-09-24 19:00:12 +0530

    Time: 0 ms (100%), Space: 9.4 MB (40.57%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char,int>mp;
+        unordered_map<char,int> mp;
+
         for(auto ch : s) {
             mp[ch]++;
         }
-        int sum=0;
-        int maxodd=0;
-        char ch;
-        for(auto [k,v]:mp){
-            if(v%2!=0&&v>maxodd){
-                maxodd=v;
-                ch=k;
+
+        int sum = 0;
+        int maxodd = 0;
+        char ch = '\0';
+
+        for(auto [k,v] : mp) {
+            if(v % 2 != 0 && v > maxodd) {
+                maxodd = v;
+                ch = k;
             }
         }
-        for(auto [k,v]:mp){
-            if(k==ch){
-                sum+=v;
+
+        for(auto [k,v] : mp) {
+            if(k == ch) {
+                sum += v;
                 continue;
             }
-            if(v%2==0){
-                sum+=v;
+
+            if(v % 2 == 0) {
+                sum += v;
             }
-            else{
-                sum+=v-1;
+            else {
+                sum += v - 1;
             }
         }
+
         return sum;
-
-
     }
 };
